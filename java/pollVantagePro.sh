6b75e6eda04d5f2ab91b540cad2b02d8f3f95399
Tom Mitchell <tom@tom.org>
2007-01-06 01:15:35 +0000

    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@609 8d57574f-b8f8-0310-901a-96530c98ddf4

---
+++ java/pollVantagePro.sh
@@ -0,0 +1,11 @@
+JAVA_HOME=/usr/java/jdk1.5.0_05
+
+PATH=$JAVA_HOME/bin:$PATH
+while true 
+do
+	ruby ./rails/oldWeather/components/noaa.rb & 2>/dev/null
+  java -noverify -cp lib/spring.jar:lib/commons-logging.jar:lib/log4j.jar:classes:lib/mysql-connector-java.jar:lib/comm.jar:lib/commons-collections.jar:lib/commons-dbcp.jar:lib/commons-pool.jar:lib/jcommon.jar:lib/jfreechart.jar:lib/axis.jar:lib/jaxrpc.jar:lib/commons-discovery-0.2.jar:lib/saaj.jar:lib/wsdl4j-1.5.1.jar -Dlog4j.configuration=file:///home/tom/dev/weather/log4j.properties uk.me.jstott.jweatherstation.Main 
+  
+  sleep 30
+done
+
