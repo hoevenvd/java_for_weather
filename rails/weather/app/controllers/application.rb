80925d5ce9967636baf0dde8ddbf146bf89f6d83
Tom Mitchell <tom@tom.org>
2007-01-10 14:27:16 +0000

    disable sessions
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@627 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/weather/app/controllers/application.rb
+++ rails/weather/app/controllers/application.rb
@@ -1,5 +1,6 @@
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
+  session :off
   #ActiveRecord::Base.verification_timeout = 300 # get ActiveRecord to check w/ the db every 5 mins
 end
