123ce804c2a92216ddf39da1ebfe754bf4ca308e
Tom Mitchell <tom@tom.org>
2007-09-02 11:42:15 +0000

    change to run from cron instead as a daemon

--- rails/weather/components/noaa.rb
+++ rails/weather/components/noaa.rb
@@ -7,50 +7,43 @@ include REXML
 
 class CacheWriter
 
-  DELAY = 600
   LOCATION = "01915-obs"
-	URL = 'www.weather.gov'
-	PREFIX = "/data/current_obs/"
-	PORT = 80
-	POSTFIX = ".xml"
-	STATION = "KBVY"
+  URL = 'www.weather.gov'
+  PREFIX = "/data/current_obs/"
+  PORT = 80
+  POSTFIX = ".xml"
+  STATION = "KBVY"
   CACHE_URL = '192.168.1.3:11211'
   CACHE_DEBUG = false
 
   def CacheWriter.get_cache
     cache = MemCache::new  CACHE_URL,
-		                       :debug => CACHE_DEBUG,
-		                       :c_threshold => 100_000,
-		                       :compression => false
+      :debug => CACHE_DEBUG,
+      :c_threshold => 100_000,
+      :compression => false
   end
 
-	def CacheWriter.write_entry(key, value)
-		CacheWriter.get_cache[key] = value	  
-	end
+  def CacheWriter.write_entry(key, value)
+    CacheWriter.get_cache[key] = value	  
+  end
 	
-	def CacheWriter.read_entry(key)
-		CacheWriter.get_cache[key]	  
-	end
+  def CacheWriter.read_entry(key)
+    CacheWriter.get_cache[key]	  
+  end
 
-	while :true
-		begin
-      log = Logger.new(STDOUT)
-      log.level = Logger::DEBUG
-			h = Net::HTTP.new(URL, PORT)
-			resp, data = h.get(PREFIX + STATION + POSTFIX, nil)
-			doc = Document.new(data)
-			value = { 'weather' => doc.elements[1].elements["weather"].text }
-			date_str = doc.elements[1].elements["observation_time"].text
-			value['observation_time'] = date_str
-			value['visibility_mi'] = doc.elements[1].elements["visibility_mi"].text.to_f
-			# log.debug(value)
-			write_entry(LOCATION, value)
-			sleep DELAY
-
-		rescue Exception
-			log.error($!)
-			sleep 60
-
-			end
-		end
+  begin
+    log = Logger.new(STDOUT)
+    log.level = Logger::DEBUG
+    h = Net::HTTP.new(URL, PORT)
+    resp, data = h.get(PREFIX + STATION + POSTFIX, nil)
+    doc = Document.new(data)
+    value = { 'weather' => doc.elements[1].elements["weather"].text }
+    date_str = doc.elements[1].elements["observation_time"].text
+    value['observation_time'] = date_str
+    value['visibility_mi'] = doc.elements[1].elements["visibility_mi"].text.to_f
+    # log.debug(value)
+    write_entry(LOCATION, value)
+  rescue Exception
+    log.error($!)
+  end
 end
