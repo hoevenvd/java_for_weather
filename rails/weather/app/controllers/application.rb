6b9d1ba49e8287a3e8e1db232477b1ed78b93514
Tom Mitchell <tom@tom.org>
2008-02-16 17:32:57 +0000

    sparklines test

--- rails/weather/app/controllers/application.rb
+++ rails/weather/app/controllers/application.rb
@@ -1,6 +1,9 @@
+require_dependency 'sparklines'
+
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
+  helper :sparklines
   session :off
   #ActiveRecord::Base.verification_timeout = 300 # get ActiveRecord to check w/ the db every 5 mins
 end
