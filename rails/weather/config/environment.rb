c9bcb3b41c0c7c9af6e839fbc991d22ad05ad7fb
Tom Mitchell <tom@tom.org>
2008-07-14 01:35:48 +0000

    get rid of forced environment

--- rails/weather/config/environment.rb
+++ rails/weather/config/environment.rb
@@ -4,7 +4,7 @@
 # you don't control web/app server and can't set it the proper way
 
 
-ENV['RAILS_ENV'] ||= 'development'
+#ENV['RAILS_ENV'] ||= 'development'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
