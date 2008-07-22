387f32d079a7c0cc189ccbd17ceeb13f6b185ac1
Tom Mitchell <tom@tom.org>
2008-07-22 02:22:31 +0000

    servers deployment file
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@855 8d57574f-b8f8-0310-901a-96530c98ddf4

---
+++ rails/weather/config/deploy/servers-services.rb
@@ -0,0 +1,77 @@
+set :application, "weather"
+
+set :user, "weather"
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "identity")] 
+#ssh_options[:port] = 7822
+
+# fixes host verification problem
+default_run_options[:pty] = true
+
+set :use_sudo, false
+
+# Site5 blocks execution of scripts (i.e., dispatch.fcgi) that are
+# group writable.  By default, Capistrano sets everything group
+# writable.  This stops that.
+set :group_writable, false
+
+set :repository,  "http://tom@home.tom.org:8081/svn-repos/weather/branches/site5/rails/#{application}"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "~/apps/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :subversion
+#set :scm_username, "tom"
+#set :scm_password, proc{Capistrano::CLI.password_prompt('SVN pass:')} 
+
+set :deploy_via, :export 
+
+role :app, "servers"
+role :web, "servers"
+role :db,  "servers", :primary => true
+
+#todo: copy database.yml
+# cp ~/apps/weather/config/database.yml ~/cap/weather/shared/system/
+# make system the permanent place and do a ln on deployment
+#desc "Symlink config.yml and database.yml from shared to  current directory
+#      since it should not be kept in version control"
+task :symlink_config_yml, :roles => :app do
+  run "ln -nsf #{shared_path}/config/database.yml
+       #{release_path}/config/database.yml"
+  run "ln -nsf #{shared_path}/config/config.yml
+       #{release_path}/config/config.yml"
+end
+
+#desc "Symlink root directory under public_html"
+task :symlink_public, :roles => :app do
+#  run "ln -nsf #{current_path}/public
+#       /var/www/wx"
+end
+
+# 
+#desc "chmod +x dispatch.fcgi"
+task :chmod_files, :roles => :app do
+  run "chmod +x #{release_path}/script/*"
+end
+
+after 'deploy:update_code', 'chmod_files', 'symlink_config_yml', 'symlink_public'
+
+namespace(:deploy) do
+  desc "Shared dispatch.fcgi restart"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+desc "tail -f development log"
+task :tail_dev_log, :roles => :app do 
+  stream "tail -f #{shared_path}/log/development.log" 
+end 
+
+desc "tail -f production log"
+task :tail_prod_log, :roles => :app do
+  stream "tail -f #{shared_path}/log/production.log"
+end
