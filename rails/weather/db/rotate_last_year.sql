a79cbc23fffd1ecd925be994e959b8af7773b529
Tom Mitchell <tom@tom.org>
2008-11-13 01:41:24 +0000

    move archive_records older than 90 days into a table for old records
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@953 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/weather/db/rotate_last_year.sql
+++ rails/weather/db/rotate_last_year.sql
@@ -1,7 +1,5 @@
-#run once a year to keep the regular database smaller
-# keep this and years data 
+#run once a month to keep the regular database smaller
 
 insert into old_archive_records 
-  select * from archive_records where date < '2008-01-01';
-delete from archive_records where date < '2008-01-01';
-
+  select * from archive_records where date < date_sub(CURDATE(), INTERVAL 90 day);
+delete from archive_records where date < date_sub(CURDATE(), INTERVAL 90 day);
