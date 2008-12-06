6f82c9fc6a08105424cebaf95f5cdc04720704df
Tom Mitchell <tom@tom.org>
2008-12-06 19:08:21 +0000

    do the rolling archive inside a database transaction
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@972 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/weather/db/rotate_old_archive_records.sql
+++ rails/weather/db/rotate_old_archive_records.sql
@@ -1,5 +1,6 @@
 #run once a month to keep the regular database smaller
-
+start transaction;
 insert into old_archive_records 
   select * from archive_records where date < date_sub(CURDATE(), INTERVAL 90 day);
 delete from archive_records where date < date_sub(CURDATE(), INTERVAL 90 day);
+commit;
