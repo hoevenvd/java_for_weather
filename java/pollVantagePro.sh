33ba90dec877deff6deab0e773583a33c08ae596
Tom Mitchell <tom@tom.org>
2009-01-03 12:59:12 +0000

    switch to port 80
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1008 8d57574f-b8f8-0310-901a-96530c98ddf4

--- java/pollVantagePro.sh
+++ java/pollVantagePro.sh
@@ -1,6 +1,10 @@
-JAVA_HOME=/usr/java/jdk1.5.0_05
+WX_HOME=/home/tom/poller
+JAVA_HOME=/usr/jdk1.6.0_10
 
 PATH=$JAVA_HOME/bin:$PATH
+
+cd $WX_HOME
+
 while true 
 do
   java -noverify -cp .:lib/spring.jar:lib/commons-logging.jar:lib/log4j.jar:classes:lib/mysql-connector-java.jar:lib/comm.jar:lib/commons-collections.jar:lib/commons-dbcp.jar:lib/commons-pool.jar:lib/jcommon.jar:lib/jfreechart.jar:lib/axis.jar:lib/jaxrpc.jar:lib/commons-discovery-0.2.jar:lib/saaj.jar:lib/wsdl4j-1.5.1.jar org.tom.weather.comm.Main
