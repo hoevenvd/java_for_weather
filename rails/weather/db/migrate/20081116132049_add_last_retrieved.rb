ff1206a6079d97a63d7b33eef49f2262399b9ea1
Tom Mitchell <tom@tom.org>
2009-01-31 22:41:31 +0000

    get rid of old migrations and regen schema.rb

--- rails/weather/db/migrate/20081116132049_add_last_retrieved.rb
+++
@@ -1,9 +0,0 @@
-class AddLastRetrieved < ActiveRecord::Migration
-  def self.up
-    add_column :noaa_forecasts, :last_retrieved, :datetime 
-  end
-
-  def self.down
-    remove_column :noaa_forecasts, :last_retrieved 
-  end
-end
