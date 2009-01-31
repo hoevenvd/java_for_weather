319e4f61ff5c7c63b6cbe5cdae80be815cddcc00
Tom Mitchell <tom@tom.org>
2009-01-31 22:41:31 +0000

    get rid of old migrations and regen schema.rb
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1034 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/weather/db/migrate/20080913095415_rename_solar_radation_column_to_average.rb
+++
@@ -1,9 +0,0 @@
-class RenameSolarRadationColumnToAverage < ActiveRecord::Migration
-  def self.up
-    rename_column "archive_records", "solar_radiation", "average_solar_radiation"
-  end
-
-  def self.down
-    rename_column "archive_records", "average_solar_radation", "solar_radiation"
-  end
-end
--- rails/weather/db/migrate/20081108211516_remove_past_summary_id.rb
+++
@@ -1,9 +0,0 @@
-class RemovePastSummaryId < ActiveRecord::Migration
-  def self.up
-    remove_column :past_summaries, :past_summary_id
-  end
-
-  def self.down
-    add_column :past_summaries, :past_summary_id
-  end
-end
