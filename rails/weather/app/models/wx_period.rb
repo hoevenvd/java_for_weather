50874b26cbbaa7231b57a296eef8bd57bd565d1e
Tom Mitchell <tom@tom.org>
2009-02-19 23:55:10 +0000

    remove comments
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1042 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/weather/app/models/wx_period.rb
+++ rails/weather/app/models/wx_period.rb
@@ -93,10 +93,10 @@ class WxPeriod < Period
   end
   
   def WxPeriod.query(pd)
-    rs = ArchiveRecord.find_by_sql("select round(avg(average_dewpoint),1) as avgDewpoint, 
-                                       round(avg(outside_humidity),0) as avgHumidity, 
-                                       round(avg(pressure),2) as avgPressure, round(avg(outside_temp),1) as avgTemp, 
-                                       round(avg(average_wind_speed),1) as avgWindspeed, round(avg(average_apparent_temp),0) as avgWindchill, 
+    rs = ArchiveRecord.find_by_sql("select avg(average_dewpoint) as avgDewpoint, 
+                                       avg(outside_humidity) as avgHumidity, 
+                                       avg(pressure) as avgPressure, avg(outside_temp) as avgTemp, 
+                                       avg(average_wind_speed)as avgWindspeed, avg(average_apparent_temp) as avgWindchill, 
                                        max(average_dewpoint) as hiDewpoint, max(high_wind_speed) as hiWindspeed, 
                                        max(outside_humidity) as hiHumidity, max(pressure) as hiPressure, max(high_outside_temp) as hiTemp, 
                                        max(average_apparent_temp) as hiWindchill, min(average_dewpoint) as lowDewpoint,
