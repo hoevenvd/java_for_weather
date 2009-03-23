181189a8bf4b74c571d3080a9a98366d524bc6b2
Tom Mitchell <tom@tom.org>
2009-03-23 22:26:45 +0000

    move to passenger - muck with htaccess and fix deploy.rb for site5-dev to work

--- rails/weather/config/deploy/site5-dev.rb
+++ rails/weather/config/deploy/site5-dev.rb
@@ -1,5 +1,7 @@
 set :application, "weather"
 
+set :deploy_dir, "/weather"
+
 set :user, "henry"
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")] 
 #ssh_options[:port] = 7822
@@ -52,7 +54,12 @@ end
 #desc "Symlink root directory under public_html"
 task :symlink_public, :roles => :app do
   run "ln -nsf #{current_path}/public
-       /home/#{user}/public_html"
+       /home/#{user}/public_html/#{deploy_dir}"
+
+# fixup .htaccess for passenger
+# example can be found in config/dot_htaccess_passenger
+  run "cp #{shared_path}/config/dot_htaccess
+       #{release_path}/public/.htaccess"
 end
 
 after 'deploy:update_code', 'symlink_config_yml', 'symlink_public'
@@ -60,7 +67,6 @@ after 'deploy:update_code', 'symlink_config_yml', 'symlink_public'
 namespace(:deploy) do
   desc "Shared phusion passenger restart"
   task :restart, :roles => :app do
-    run "rm #{current_path}/public/.htaccess"
     run "touch #{current_path}/tmp/restart.txt"
   end
 end
