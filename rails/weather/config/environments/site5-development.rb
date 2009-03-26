0adf52bff32f857dde262d8b28ce1f768bd2e030
Tom Mitchell <tom@tom.org>
2009-03-26 23:42:55 +0000

    try to set asset_host to serve static images
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1086 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/weather/config/environments/site5-development.rb
+++ rails/weather/config/environments/site5-development.rb
@@ -15,3 +15,7 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+# Enable serving of images, stylesheets, and javascripts from an asset server
+config.action_controller.asset_host                  = "http://henrymitchell.org/weather"
+
