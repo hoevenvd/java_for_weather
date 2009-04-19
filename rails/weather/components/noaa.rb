93a4afea7f098541f7ec60869de3b66d6ace543b
Tom Mitchell <tom@tom.org>
2009-04-19 19:47:40 +0000

    use open-uri instead of plain http classes for noaa utulities to be able to handle redirects

--- rails/weather/components/noaa.rb
+++ rails/weather/components/noaa.rb
@@ -1,8 +1,8 @@
 require 'logger'
 require 'date'
 require 'net/http'
+require 'open-uri'
 require 'rexml/document'
-require 'noaa_forecast'
 include REXML
 
 # http://www.weather.gov/xml/current_obs/KBVY.xml
@@ -12,32 +12,28 @@ class NOAAConditionsWriter
   log.level = Logger::INFO
 
   LOCATION_FORECAST = "01915-forecast"
-  URL = 'www.weather.gov'
+  HOST = 'www.weather.gov'
   PREFIX = "/xml/current_obs/"
   PORT = 80
   POSTFIX = ".xml"
   STATION = ARGV[0] # Appconfig.noaa_location # "KBVY" # 
-
-  begin
-    h = Net::HTTP.new(URL, PORT)
-    resp, data = h.get(PREFIX + STATION + POSTFIX, nil)
-    log.debug(resp)
-    log.debug(data)
-    doc = Document.new(data)
-    location = doc.elements[1].elements["station_id"].text
-    as_of = Time.rfc822(doc.elements[1].elements["observation_time_rfc822"].text).utc
-    record = NoaaConditions.find_by_as_of_and_location(as_of, location)
-    log.debug(record)
-    if (record.nil?)
-      conditions = NoaaConditions.new
-      conditions.conditions_xml = data
-      conditions.location = location
-      conditions.as_of = as_of
-      conditions.conditions = doc.elements[1].elements["weather"].text
-      conditions.visibility = doc.elements[1].elements["visibility_mi"].text.to_i
-      conditions.save!
-      log.debug(conditions)
-    end
+  URL = "http://" + HOST + PREFIX + STATION + POSTFIX
+  f = open(URL) # open-uri - treat the URL as an input stream
+  data = f.read
+  log.debug(data)
+  doc = Document.new(data)
+  location = doc.elements[1].elements["station_id"].text
+  as_of = Time.rfc822(doc.elements[1].elements["observation_time_rfc822"].text).utc
+  record = NoaaConditions.find_by_as_of_and_location(as_of, location)
+  log.debug(record)
+  if (record.nil?)
+    conditions = NoaaConditions.new
+    conditions.conditions_xml = data
+    conditions.location = location
+    conditions.as_of = as_of
+    conditions.conditions = doc.elements[1].elements["weather"].text
+    conditions.visibility = doc.elements[1].elements["visibility_mi"].text.to_i
+    conditions.save!
+    log.debug(conditions)
   end
-
 end
