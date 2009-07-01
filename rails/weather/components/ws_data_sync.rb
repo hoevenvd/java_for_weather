f60188550c544f6c9234c8674de4b0d4cd0186ed
Tom Mitchell <tom@tom.org>
2009-07-01 01:48:11 +0000

    add an infinite loop

--- rails/weather/components/ws_data_sync.rb
+++ rails/weather/components/ws_data_sync.rb
@@ -13,39 +13,41 @@ log.debug(dest_soap)
 
 error_count = 0
 
-begin
-  15.times do
-    log.debug("getting conditions.")
-    t = Time.now
-    conditions = src_soap.GetCurrentConditions("01915")
-    log.debug(conditions)
-    log.debug(Time.now - t)
-    log.debug("writing")
-    t = Time.now
-    dest_soap.PutCurrentConditions("wx", "01915", conditions)
-    log.debug("done.")
-    log.debug(Time.now - t)
-    error_count = 0
-    sleep 2
+while true
+  begin
+    15.times do
+      log.debug("getting conditions.")
+      t = Time.now
+      conditions = src_soap.GetCurrentConditions("01915")
+      log.debug(conditions)
+      log.debug(Time.now - t)
+      log.debug("writing")
+      t = Time.now
+      dest_soap.PutCurrentConditions("wx", "01915", conditions)
+      log.debug("done.")
+      log.debug(Time.now - t)
+      error_count = 0
+      sleep 2
+    end
+
+    last_archive = dest_soap.GetLastArchive("01915")
+    log.debug(last_archive.date)
+
+    archive_structs = src_soap.GetArchiveSince("wx", "01915", last_archive.date)
+    log.debug("returned #{archive_structs.length} entries")
+
+    archive_structs.each do |s|
+      t = Time.now
+      dest_soap.PutArchiveEntry("wx", "01915", s)
+      log.debug(s.date)
+      log.debug(Time.now - t)
+    end
+  rescue
+    log.error($!)
+    log.error("sleeping...")
+    sleep 60
+    exit 1 unless error_count < 25
+    error_count += 1
+    retry
   end
-
-  last_archive = dest_soap.GetLastArchive("01915")
-  log.debug(last_archive.date)
-
-  archive_structs = src_soap.GetArchiveSince("wx", "01915", last_archive.date)
-  log.debug("returned #{archive_structs.length} entries")
-
-  archive_structs.each do |s|
-    t = Time.now
-    dest_soap.PutArchiveEntry("wx", "01915", s)
-    log.debug(s.date)
-    log.debug(Time.now - t)
-  end
-rescue
-  log.error($!)
-  log.error("sleeping...")
-  sleep 60
-  exit 1 unless error_count < 25
-  error_count += 1
-  retry
 end
