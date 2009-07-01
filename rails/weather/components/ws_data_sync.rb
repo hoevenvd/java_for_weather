e1f6d50636d3c58ef1a2a3b2f09cfe559fddb371
Tom Mitchell <tom@tom.org>
2009-07-01 01:04:50 +0000

    parameterize src and dest urls and add more conservative error handling (sit and wait for a minute on any error)
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1116 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/weather/components/ws_data_sync.rb
+++ rails/weather/components/ws_data_sync.rb
@@ -1,48 +1,51 @@
 require 'soap/wsdlDriver'
 
 log = Logger.new(STDOUT)
-log.level = Logger::WARN
+log.level = Logger::DEBUG
 
-src_url="http://wx/weather/wsdl"
-src_soap = SOAP::WSDLDriverFactory.new(src_url).create_rpc_driver
+SRC = AppConfig.sync_src
+src_soap = SOAP::WSDLDriverFactory.new(SRC).create_rpc_driver
 log.debug(src_soap)
-dest_url="http://tommitchell.net/weather/weather/wsdl"
-dest_soap = SOAP::WSDLDriverFactory.new(dest_url).create_rpc_driver
+
+DEST = AppConfig.sync_dest
+dest_soap = SOAP::WSDLDriverFactory.new(DEST).create_rpc_driver
 log.debug(dest_soap)
+
 error_count = 0
 
-while true do
-
-  begin
-    15.times do
-      log.debug("getting conditions.")
-      t = Time.now
-      conditions = src_soap.GetCurrentConditions("01915")
-      log.debug(conditions)
-      log.debug(Time.now - t)
-      log.debug("writing")
-      t = Time.now
-      dest_soap.PutCurrentConditions("wx", "01915", conditions)
-      log.debug("done.")
-      log.debug(Time.now - t)
-      error_count = 0
-      sleep 2
-    end
-
-    last_archive = dest_soap.GetLastArchive("01915")
-    log.debug(last_archive.date)
-
-    archive_structs = src_soap.GetArchiveSince("wx", "01915", last_archive.date)
-    log.debug("returned #{archive_structs.length} entries")
-
-    archive_structs.each do |s|
-      t = Time.now
-      dest_soap.PutArchiveEntry("wx", "01915", s)
-      log.debug(s.date)
-      log.debug(Time.now - t)
-    end
-  rescue
-    exit 1 unless error_count < 25
-    error_count += 1
+begin
+  15.times do
+    log.debug("getting conditions.")
+    t = Time.now
+    conditions = src_soap.GetCurrentConditions("01915")
+    log.debug(conditions)
+    log.debug(Time.now - t)
+    log.debug("writing")
+    t = Time.now
+    dest_soap.PutCurrentConditions("wx", "01915", conditions)
+    log.debug("done.")
+    log.debug(Time.now - t)
+    error_count = 0
+    sleep 2
   end
+
+  last_archive = dest_soap.GetLastArchive("01915")
+  log.debug(last_archive.date)
+
+  archive_structs = src_soap.GetArchiveSince("wx", "01915", last_archive.date)
+  log.debug("returned #{archive_structs.length} entries")
+
+  archive_structs.each do |s|
+    t = Time.now
+    dest_soap.PutArchiveEntry("wx", "01915", s)
+    log.debug(s.date)
+    log.debug(Time.now - t)
+  end
+rescue
+  log.error($!)
+  log.error("sleeping...")
+  sleep 60
+  exit 1 unless error_count < 25
+  error_count += 1
+  retry
 end
