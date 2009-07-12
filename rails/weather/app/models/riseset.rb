369aa21ddb36828526fc8f064c1d7af9b0814523
Tom Mitchell <tom@tom.org>
2009-07-12 14:32:13 +0000

    more timezone handling
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1125 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/weather/app/models/riseset.rb
+++ rails/weather/app/models/riseset.rb
@@ -3,13 +3,13 @@ class Riseset < ActiveRecord::Base
     date = Time.now.utc
     r = Riseset.find_by_location_and_month_and_day(AppConfig.climate_location,
             date.month, date.day)
-    return Time.gm(date.year, date.month, date.day, r.rise.hour, r.rise.min).localtime
+    return Time.gm(date.year, date.month, date.day, r.rise.hour, r.rise.min).in_time_zone
   end
 
   def self.today_set
     date = Time.now.utc
     r = Riseset.find_by_location_and_month_and_day(AppConfig.climate_location,
             date.month, date.day)
-    return Time.gm(date.year, date.month, date.day, r.set.hour, r.set.min).localtime
+    return Time.gm(date.year, date.month, date.day, r.set.hour, r.set.min).in_time_zone
   end
 end
