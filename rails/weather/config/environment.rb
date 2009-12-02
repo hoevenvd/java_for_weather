e54928ebc9fb0a31ceadad6c013a494db1781a19
Tom Mitchell <tom@tom.org>
2009-12-02 15:59:23 +0000

    freeze rack into app
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1206 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/weather/config/environment.rb
+++ rails/weather/config/environment.rb
@@ -7,7 +7,7 @@
 ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 #RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
@@ -84,6 +84,9 @@ Rails::Initializer.run do |config|
 
   config.cache_store = :file_store, "#{RAILS_ROOT}/tmp/cache"
   
+# config rack as a frozen gem
+  config.gem 'rack'
+
 end
 
 #require 'actionwebservice'
