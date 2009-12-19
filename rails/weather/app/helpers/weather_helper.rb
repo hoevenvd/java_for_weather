3358c70c20aac9f98b627bae9ad45f74d166fb01
Tom Mitchell <tom@tom.org>
2009-12-19 21:08:45 +0000

    dont send solar radiation as 32767 if it isnt really there
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1224 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/weather/app/helpers/weather_helper.rb
+++ rails/weather/app/helpers/weather_helper.rb
@@ -82,7 +82,7 @@ module WeatherHelper
     post_url += "&dailyrainin=" + CGI::escape(new_sample[:dailyrainin].to_s)
     post_url += "&baromin=" + CGI::escape(new_sample[:baromin].to_s)
     post_url += "&dewptf=" + CGI::escape(new_sample[:dewptf].to_s)
-    post_url += "&solarradiation=" + CGI::escape(new_sample[:solarradiation].to_s)
+    post_url += "&solarradiation=" + CGI::escape(new_sample[:solarradiation].to_s) unless new_sample[:solarradiation] == 32767
 #    post_url += "&weather=" + CGI::escape(noaa.conditions) unless noaa.nil?
 #    post_url += "&visibility=" + CGI::escape(noaa.visibility.to_s) unless noaa.nil?
     post_url += "&softwaretype=" + CGI::escape(new_sample[:softwaretype].to_s)
