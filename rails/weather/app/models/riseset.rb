2060036d22fe3414ac8570bb8381d6aad7248d7d
Tom Mitchell <tom@tom.org>
2010-02-23 11:02:21 +0000

    rearrange climate info and add sunrise and set in a week and in a month
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1264 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/weather/app/models/riseset.rb
+++ rails/weather/app/models/riseset.rb
@@ -1,18 +1,4 @@
 class Riseset < ActiveRecord::Base
-  def self.today_rise
-    date = Time.now.utc
-    r = Riseset.find_by_location_and_month_and_day(AppConfig.climate_location,
-            date.month, date.day)
-    return Time.gm(date.year, date.month, date.day, r.rise.hour, r.rise.min).in_time_zone
-  end
-
-  def self.today_set
-    date = Time.now.utc
-    r = Riseset.find_by_location_and_month_and_day(AppConfig.climate_location,
-            date.month, date.day)
-    return Time.gm(date.year, date.month, date.day, r.set.hour, r.set.min).in_time_zone
-  end
-
   def self.riseset date
     r = Riseset.find_by_location_and_month_and_day(AppConfig.climate_location,
             date.month, date.day)
