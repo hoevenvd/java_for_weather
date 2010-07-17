aa6e04837be587334007b63a5d33c557cf08e374
Tom Mitchell <tom@tom.org>
2010-07-17 09:41:37 +0000

    new first pass at caching for faster page loads
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1503 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/weather/app/models/past_summary.rb
+++ rails/weather/app/models/past_summary.rb
@@ -1,3 +1,4 @@
 class PastSummary < ActiveRecord::Base
   include WxUtils
+  named_scope :current_periods, lambda {{:conditions => ["enddate > ? and location = \'#{AppConfig.location}\'", Time.now.utc]}}
 end
