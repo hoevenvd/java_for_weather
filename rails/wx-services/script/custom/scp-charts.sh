c10bc332cf548c9fee82c8b4bd6f392a3c455f86
Tom Mitchell <tom@tom.org>
2010-09-02 14:28:23 +0000

    rrd-enabled services

--- rails/wx-services/script/custom/scp-charts.sh
+++ rails/wx-services/script/custom/scp-charts.sh
@@ -2,5 +2,7 @@
 
 export CHARTS_DIR=/home/tom/tmp/charts
 
+
 scp -q $CHARTS_DIR/*.jpg tomorg@tommitchell.net:/home/tomorg/public_html/images/weather/charts/
 
+scp -q $CHARTS_DIR/*.png tomorg@tommitchell.net:/home/tomorg/public_html/images/weather/charts/
