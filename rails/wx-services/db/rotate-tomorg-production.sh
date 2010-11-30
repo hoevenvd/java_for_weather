88200ea580c1435915c8f530baf97eccd4f283f0
Tom Mitchell <tom@tom.org>
2010-11-30 11:12:21 +0000

    change hostname to resolves to site5
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1622 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/wx-services/db/rotate-tomorg-production.sh
+++ rails/wx-services/db/rotate-tomorg-production.sh
@@ -1,1 +1,1 @@
-mysql --user tomorg_wxprod -p -h www.tom.org tomorg_weatherproduction < db/rotate_old_archive_records.sql
+mysql --user tomorg_wxprod -p -h tommitchell.net tomorg_weatherproduction < db/rotate_old_archive_records.sql
