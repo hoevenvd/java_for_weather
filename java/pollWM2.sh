524f85af8064b6751bc671e402bf59cfb10bfc3a
Tom Mitchell <tom@tom.org>
2010-12-05 20:03:50 +0000

    remove java graphing stuff

--- java/pollWM2.sh
+++ java/pollWM2.sh
@@ -10,7 +10,7 @@ cp -f weather.xml.wm weather.xml
 
 while true 
 do
-  java -Duser.timezone=$TZ -noverify -cp .:lib/spring.jar:lib/commons-logging.jar:lib/log4j.jar:classes:lib/mysql-connector-java.jar:lib/comm.jar:lib/commons-collections.jar:lib/commons-dbcp.jar:lib/commons-pool.jar:lib/jcommon.jar:lib/jfreechart.jar:lib/axis.jar:lib/jaxrpc.jar:lib/commons-discovery-0.2.jar:lib/saaj.jar:lib/wsdl4j-1.5.1.jar org.tom.weather.comm.Main
+  java -Duser.timezone=$TZ -noverify -cp .:lib/spring.jar:lib/commons-logging.jar:lib/log4j.jar:classes:lib/comm.jar:lib/axis.jar:lib/jaxrpc.jar:lib/commons-discovery-0.2.jar:lib/saaj.jar:lib/wsdl4j-1.5.1.jar org.tom.weather.comm.Main
   sleep 30
 done
 
