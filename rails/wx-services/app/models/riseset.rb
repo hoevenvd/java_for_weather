a614445c70f7e21d10161051f43c0bddce0cd5c5
Tom Mitchell <tom@tom.org>
2011-01-07 16:34:29 +0000

    only show he webcam when it is light
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1627 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/wx-services/app/models/riseset.rb
+++ rails/wx-services/app/models/riseset.rb
@@ -1,9 +1,17 @@
 class Riseset < ActiveRecord::Base
-  def self.riseset date
+  def self.riseset(date)
     r = Riseset.find_by_location_and_month_and_day(AppConfig.climate_location,
             date.month, date.day)
     { :rise => Time.gm(date.year, date.month, date.day, r.rise.hour, r.rise.min),
       :set => Time.gm(date.year, date.month, date.day, r.set.hour, r.set.min) }
+  end
+
+  # a very conservative measurement of light and dark. when in doubt, its light.
+  def self.dark?(location, date)
+    date = date.utc
+    r = Riseset.find_by_location_and_month_and_day(AppConfig.climate_location,
+            date.month, date.day)
+    return ((date.hour < r.rise.hour - 1) or (date.hour >= r.set.hour + 1))
   end
 end
 #t = Time.gm(Time.now.year, Time.now.month, Time.now.day, r[:set].hour, r[:set].min).localtime
