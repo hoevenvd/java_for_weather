86a4d57b08a847cfe0ee49f6ccda4f966b3f9430
Tom Mitchell <tom@tom.org>
2011-01-07 16:32:24 +0000

    only show he webcam when it is light
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1625 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/wx-webapp/test/unit/riseset_test.rb
+++ rails/wx-webapp/test/unit/riseset_test.rb
@@ -6,4 +6,15 @@ class RisesetTest < ActiveSupport::TestCase
   def test_truth
     assert true
   end
+# id	location	month	day	rise	      set
+# 733	  01915	    1	   1	12:14:00	21:22:00
+  def test_dark?
+    d = Time.local(2011, 1, 3, 17, 00)
+    # 17:00 EST is 22:00 UTC, right?
+    assert(Riseset.dark?("01915", d))
+    d = Time.local(2011, 1, 3, 5, 00)
+
+    assert(Riseset.dark?("01915", d))
+    assert(Riseset.dark?("01915", d.utc))
+  end
 end
