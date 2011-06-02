8bf9ecee932a24ed80e84482beb01018cefa1bf4
Tom Mitchell <tom@tom.org>
2011-06-02 09:55:08 +0000

    fox dte format for wunderground, have 01915 post to experimental station
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1751 8d57574f-b8f8-0310-901a-96530c98ddf4

--- gae/wu.py
+++ gae/wu.py
@@ -14,20 +14,20 @@ def post(condition=None, archive=None, station=None, password=None):
     return
   # get last hour's rain from the archives
   if condition:
-    rainin = archiveservice.last_hour_rain(condition.location, c.date.astimezone(pytz.utc))
+    #rainin = archiveservice.last_hour_rain(condition.location, c.date.astimezone(pytz.utc))
     w = wunderground.Wunderground(station, password, 2.5)
-    w.set(dateutc=str(datetime.datetime.utcnow()), pressure=condition.pressure,
-          rainday=condition.day_rain,dewpoint=condition.dewpoint,humidity=condition.outside_humidity,
-          tempf=condition.outside_temp,winddir=condition.wind_direction,rainin=condition.rain_rate,
-          windspeed=condition.wind_speed,solarradiation=condition.solar_radiation)
+    w.set(dateutc=datetime.datetime.utcnow().replace(tzinfo=pytz.utc).strftime("%Y-%m-%d %H:%M:%S"),
+          pressure=condition.pressure,rainday=condition.day_rain,dewpoint=condition.dewpoint,
+          humidity=condition.outside_humidity,tempf=condition.outside_temp,winddir=condition.wind_direction,
+          rainin=condition.rain_rate,windspeed=condition.wind_speed,solarradiation=condition.solar_radiation)
     w.publish()
   if archive:
     rainin = archiveservice.last_hour_rain(archive.location, archive.date.astimezone(pytz.utc))
     w = wunderground.Wunderground(station, password)
     dewpoint = temps.calc_dewpoint(archive.outside_temp, archive.outside_humidity)
-    w.set(dateutc=str(archive.date), pressure=archive.pressure,
-          dewpoint=dewpoint,humidity=archive.outside_humidity,
-          tempf=archive.outside_temp,winddir=archive.prevailing_wind_direction,rainin=archive.high_rain_rate,
-          windspeed=archive.avg_wind_speed,windgust=archive.high_wind_speed,windgustdir=archive.high_wind_speed_direction,
-          solarradiation=archive.high_solar_radiation)
+    w.set(dateutc=archive.date.astimezone(pytz.utc).strftime("%Y-%m-%d %H:%M:%S"),
+          pressure=archive.pressure,dewpoint=dewpoint,humidity=archive.outside_humidity,
+          tempf=archive.outside_temp,winddir=archive.prevailing_wind_direction,
+          rainin=archive.high_rain_rate,windspeed=archive.avg_wind_speed,windgust=archive.high_wind_speed,
+          windgustdir=archive.high_wind_speed_direction,solarradiation=archive.high_solar_radiation)
     w.publish()
