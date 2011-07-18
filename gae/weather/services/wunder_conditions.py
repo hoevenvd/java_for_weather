c04ee4f7c2697a5640fc421b9315636af1a9e0e6
Tom Mitchell <tom@tom.org>
2011-07-18 18:41:28 +0000

    add conditions from wunderground
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1766 8d57574f-b8f8-0310-901a-96530c98ddf4

---
+++ gae/weather/services/wunder_conditions.py
@@ -0,0 +1,31 @@
+from google.appengine.api import memcache
+from xml.etree import ElementTree
+import urllib
+
+#http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=kbvy
+
+URL='http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query='
+
+class WunderConditions:
+  def __init__(self, as_of, weather, visibility):
+    self.as_of = as_of
+    self.weather = weather
+    self.visibility = visibility
+      
+
+class ConditionsFactory:
+  @staticmethod
+  def get(location):
+    return memcache.get(location, namespace='conditions')
+
+  @staticmethod
+  def put(location):
+    feed = urllib.urlopen(URL+location)
+    tree = ElementTree.parse(feed)
+    as_of = tree.find('/observation_time').text
+    weather = tree.find('/weather').text
+    visibility = tree.find('/visibility_mi').text
+    if as_of:
+      conditions = WunderConditions(as_of, weather, visibility)
+      memcache.set(location, conditions, time=7200, namespace='conditions')
+      return conditions
