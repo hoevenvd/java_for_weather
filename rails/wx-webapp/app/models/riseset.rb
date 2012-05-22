865352acb256e8c75d2ce12bc488a64ebdf8785e
Tom Mitchell <tom@tom.org>
2012-05-22 16:41:05 +0000

    fix riseset bug where day is wrong when set > midnight utc
    git-svn-id: svn+ssh://tommitchell.net/home/tomorg/svn/weather/trunk@1832 8d57574f-b8f8-0310-901a-96530c98ddf4

--- rails/wx-webapp/app/models/riseset.rb
+++ rails/wx-webapp/app/models/riseset.rb
@@ -1,17 +1,19 @@
 class Riseset < ActiveRecord::Base
-  def self.riseset(date)
-    r = Riseset.find_by_location_and_month_and_day(AppConfig.climate_location,
+	#TODO - bug where if the hour gmt is past midnight (say, 0, 1, or 2) then the date comparison fails and dark?
+	# thinks it is dark when it is not
+  def self.riseset(location, date)
+    r = Riseset.find_by_location_and_month_and_day(location,
             date.month, date.day)
     { :rise => Time.gm(date.year, date.month, date.day, r.rise.hour, r.rise.min),
-      :set => Time.gm(date.year, date.month, date.day, r.set.hour, r.set.min) }
+      :set => Time.gm(date.year, date.month, r.set.hour < r.rise.hour ? date.day + 1: date.day, r.set.hour, r.set.min) }
   end
 
-  # a very conservative measurement of light and dark. when in doubt, its light.
+  # a very conservative measurement of light and dark. when in doubt, it's light.
   def self.dark?(location, date)
     date = date.utc
-    r = Riseset.find_by_location_and_month_and_day(AppConfig.climate_location,
-            date.month, date.day)
-    return ((date.hour < r.rise.hour - 1) or (date.hour >= r.set.hour + 1))
+    r = Riseset.riseset(location, date)
+    #find_by_location_and_month_and_day(AppConfig.climate_location, date.month, date.day)
+    return ((date < r[:rise] - 1.hour) or (date >= r[:set] + 1.hour))
   end
 end
 #t = Time.gm(Time.now.year, Time.now.month, Time.now.day, r[:set].hour, r[:set].min).localtime
