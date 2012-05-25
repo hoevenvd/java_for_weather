f71a5ecc4c058b5bec37aeac6350d15ee955c8e9
Tom Mitchell <tom@tom.org>
2012-05-25 08:36:38 +0000

    another attempt to fix sunrise bug

--- rails/wx-webapp/app/models/riseset.rb
+++ rails/wx-webapp/app/models/riseset.rb
@@ -1,16 +1,17 @@
 class Riseset < ActiveRecord::Base
-	#TODO - bug where if the hour gmt is past midnight (say, 0, 1, or 2) then the date comparison fails and dark?
 	# thinks it is dark when it is not
   def self.riseset(location, date)
     r = Riseset.find_by_location_and_month_and_day(location,
-            date.month, date.day)
-    { :rise => Time.gm(date.year, date.month, date.day, r.rise.hour, r.rise.min),
-      :set => Time.gm(date.year, date.month, r.set.hour < r.rise.hour ? date.day + 1: date.day, r.set.hour, r.set.min) }
+            date.localtime.month, date.localtime.day)
+    t = { :rise => Time.gm(date.year, date.month, date.day, r.rise.hour, r.rise.min),
+          :set => Time.gm(date.year, date.month, date.day, r.set.hour, r.set.min) }
+    t[:set] = t[:set] + 1.day if t[:set].hour < 12
+    return t
   end
 
   # a very conservative measurement of light and dark. when in doubt, it's light.
   def self.dark?(location, date)
-    date = date.utc
+    date = date.localtime
     r = Riseset.riseset(location, date)
     #find_by_location_and_month_and_day(AppConfig.climate_location, date.month, date.day)
     return ((date < r[:rise] - 1.hour) or (date >= r[:set] + 1.hour))
