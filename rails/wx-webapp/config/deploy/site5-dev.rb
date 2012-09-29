ae37edb9934f0bbfd26cae08f9790a0f09ed64b6
Tom Mitchell <tom@tom.org>
2012-09-29 15:10:38 -0400

    change webapp to deploy from github via cap

--- rails/wx-webapp/config/deploy/site5-dev.rb
+++ rails/wx-webapp/config/deploy/site5-dev.rb
@@ -16,20 +16,18 @@ set :use_sudo, false
 # writable.  This stops that.
 set :group_writable, false
 
-set :repository,  "svn+ssh://tomorg@tommitchell.net/home/tomorg/svn/weather/trunk/rails/#{application}"
+set :deploy_subdir, "rails/wx-webapp"
+set :scm, :git
+set :deploy_via, :remote_cache
+set :repository_cache, "git_cache"
+set :ssh_options, { :forward_agent => true }
+set :repository, "git@github.com:mitct02/weather.git"
+set :branch, "master"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "~/apps/#{application}"
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-set :scm, :subversion
-# set :scm_username, "#{user}"
-#set :scm_password, proc{Capistrano::CLI.password_prompt('SVN pass:')} 
-
-set :deploy_via, :export 
 
 role :app, "henrymitchell.org"
 role :web, "henrymitchell.org"
