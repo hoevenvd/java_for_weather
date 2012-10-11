b564652f400915b9aed84e9ce15729b71379836b
Tom Mitchell <tom@tom.org>
2012-10-11 19:05:47 -0400

    wunderground script no longer sets RAILS_ENV

--- rails/wx-services/script/custom/wunderground.sh
+++ rails/wx-services/script/custom/wunderground.sh
@@ -4,7 +4,5 @@
 
 #cd ~/apps/wx-services/current
 
-export RAILS_ENV=production
-
 ./script/runner components/wunder_conditions.rb >/dev/null
 ./script/runner components/wunder_forecast.rb >/dev/null
