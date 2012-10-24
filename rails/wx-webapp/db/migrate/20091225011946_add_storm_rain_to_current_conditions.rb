911b145a30fcbbba4551b3d5919e689915ba0913
Tom Mitchell <tom@tom.org>
2012-10-24 06:31:07 -0400

    get rid of confusing database stuff in webapp. all db stuff should be in services instead

--- rails/wx-webapp/db/migrate/20091225011946_add_storm_rain_to_current_conditions.rb
+++
@@ -1,9 +0,0 @@
-class AddStormRainToCurrentConditions < ActiveRecord::Migration
-  def self.up
-    add_column :current_conditions, :storm_rain, :float
-  end
-
-  def self.down
-    remove_column :current_conditions, :storm_rain
-  end
-end
