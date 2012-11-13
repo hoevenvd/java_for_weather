d487b418d609a1870463860ebf568b163b1c970e
Tom Mitchell <tom@tom.org>
2012-11-13 05:01:20 -0500

    deploy files for srver

--- rails/wx-services/config/deploy/server-prod.rb
+++ rails/wx-services/config/deploy/server-prod.rb
@@ -1,6 +1,6 @@
 set :application, "wx-services"
 
-set :deploy_dir, "/weather"
+#set :deploy_dir, "/wx-services"
 
 set :user, "tom"
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")] 
@@ -17,26 +17,30 @@ set :use_sudo, false
 # writable.  This stops that.
 set :group_writable, false
 
-set :repository,  "svn+ssh://tomorg@tommitchell.net/home/tomorg/svn/weather/trunk/rails/#{application}"
+set :deploy_subdir, "rails/wx-services"
+set :scm, :git
+set :deploy_via, :remote_cache
+set :repository_cache, "git_cache"
+set :ssh_options, { :forward_agent => true }
+set :repository, "git@github.com:mitct02/weather.git"
+set :branch, "master"
+
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "~/apps/#{application}"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-set :scm, :subversion
-set :scm_username, "tomorg"
-#set :scm_password, proc{Capistrano::CLI.password_prompt('SVN pass:')} 
+role :app, "server"
+role :web, "server"
+role :db,  "server", :primary => true
 
-set :deploy_via, :export 
+#role :app, "dev"
+#role :web, "dev"
+#role :db,  "dev", :primary => true
 
-role :app, "raid"
-role :web, "raid"
-role :db,  "raid", :primary => true
-
-
+#desc "Symlink config.yml and database.yml from shared to  current directory
+#      since it should not be kept in version control"
 task :symlink_config_yml, :roles => :app do
   run "ln -nsf #{shared_path}/config/database.yml
        #{release_path}/config/database.yml"
@@ -55,13 +59,7 @@ namespace(:deploy) do
   end
 end
 
-desc "tail -f development log"
-task :tail_dev_log, :roles => :app do 
-  stream "tail -f #{shared_path}/log/development.log" 
-end 
-
 desc "tail -f production log"
 task :tail_prod_log, :roles => :app do
   stream "tail -f #{shared_path}/log/production.log"
 end
-
